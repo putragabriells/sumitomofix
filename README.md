113132e1aaef16aa31eadfd4692fdd28a0ef89cb
Putra Gabriel <putra@SBI.SWS.CO.JP>
2022-09-30 14:32:53 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# React based authentication page template
+
+This is my starter template for Login, Register and Forgot Password using ReactJS with simple form validation. Upon entering your credentials, the app will display a simple alert if you are logged in.
+Based on React v17.
+
+## Screenshot
+![Screenshot of login page](src/assets/screenshot.png "Screenshot of login page")
+
+## Installation
+Clone this repository
+```bash
+git clone https://github.com/christerjohansson/react-authentication-page-template.git
+```
+
+go to react-auth-template directory.
+
+In the project directory, you can run:
+
+### `npm install`
+This will install all the dependencies of the app.
+
+### `npm start`
+
+Open your browser and go to [http://localhost:3000](http://localhost:3000).
+
+The page will reload if you make any changes on the code
+
+### `npm run build`
+
+This will build the app for production.
+
+
+## Screenshots
+
+
+#### Login Form w/ invalid email validation
+#### *NOTE: to show this email error message, change the input type from "email" to "text"*
+
+![login](https://i.imgur.com/0QKNX05.png)
+
+#### Register Form
+![register](https://i.imgur.com/H8ZJOPj.png)
+
+#### Forgot Password Form
+![forgot](https://i.imgur.com/moZyR37.png)
+
---
+++ src/components/views/Login.js
@@ -0,0 +1,222 @@
+/*
+ Copyright (c) 2021 Christer Johansson of Sweden Inc.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ the Software, and to permit persons to whom the Software is furnished to do so,
+ subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Form from "../../utilities/Forms";
+// import  from "./image/logo.png";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [remember, setRemember] = useState(false);
+  const [validate, setValidate] = useState({});
+  const [showPassword, setShowPassword] = useState(false);
+  const logo = require ('./image/logo.png');
+
+  const validateLogin = () => {
+    let isValid = true;
+
+    let validator = Form.validator({
+      email: {
+        value: email,
+        isRequired: true,
+        isEmail: true,
+      },
+      password: {
+        value: password,
+        isRequired: true,
+        minLength: 6,
+      },
+    });
+
+    if (validator !== null) {
+      setValidate({
+        validate: validator.errors,
+      });
+
+      isValid = false;
+    }
+    return isValid;
+  };
+
+  const authenticate = (e) => {
+    e.preventDefault();
+
+    const validate = validateLogin();
+
+    if (validate) {
+      setValidate({});
+      setEmail("");
+      setPassword("");
+      alert("Successfully Login");
+    }
+  };
+
+  const togglePassword = (e) => {
+    if (showPassword) {
+      setShowPassword(false);
+    } else {
+      setShowPassword(true);
+    }
+  };
+
+  return (
+    <div className="row g-0 auth-wrapper">
+      <div className="col-12 col-md-1 col-lg-1 h-100 auth-background-col">
+        <div className="auth-background-holder"></div>
+        <div className="auth-background-mask"></div>
+      </div>
+      <div className="col-12 col-md-7 col-lg-6 auth-main-col text-center">
+        <div className="d-flex flex-column align-content-end">
+          <div className="auth-body mx-auto">
+          <div className="container" style={{ marginTop: "120px" }}>
+            <div className="row justify-content-center">
+                <div className="col-xl-12 col-lg-12">
+                    <div className="card border-0">
+            <p className="fw-bold card-header card-border-1" ><img src={logo}/>LOGIN ACCOUNT</p>
+            <div className="auth-form-container card-body text-start"
+              <form
+                className="auth-form"
+                method="POST"
+                onSubmit={authenticate}
+                autoComplete={"off"}
+              >
+                <div className="email mb-1 ">
+                <label for="email" class="col-sm-2 col-form-control">Email</label>
+                  <input
+                    type="email"
+                    className={`form-control ${
+                      validate.validate && validate.validate.email
+                        ? "is-invalid "
+                        : ""
+                    }`}
+                    id="email"
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+
+                  <div
+                    className={`invalid-feedback text-start ${
+                      validate.validate && validate.validate.email
+                        ? "d-block"
+                        : "d-none"
+                    }`}
+                  >
+                    {validate.validate && validate.validate.email
+                      ? validate.validate.email[0]
+                      : ""}
+                  </div>
+                </div>
+
+                <div className="password mb-1">
+                <label for="Password" class="col-sm-4 col-form-label">Password</label>
+                  <div className="input-group">
+                    <input
+                      type={showPassword ? "text" : "password"}
+                      className={`form-control ${
+                        validate.validate && validate.validate.password
+                          ? "is-invalid "
+                          : ""
+                      }`}
+                      name="password"
+                      id="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+
+                    <button
+                      type="button"
+                      className="btn btn-outline-primary btn-sm"
+                      onClick={(e) => togglePassword(e)}
+                    >
+                      <i
+                        className={
+                          showPassword ? "far fa-eye" : "far fa-eye-slash"
+                        }
+                      ></i>{" "}
+                    </button>
+
+                    <div
+                      className={`invalid-feedback text-start ${
+                        validate.validate && validate.validate.password
+                          ? "d-block"
+                          : "d-none"
+                      }`}
+                    >
+                      {validate.validate && validate.validate.password
+                        ? validate.validate.password[0]
+                        : ""}
+                    </div>
+                  </div>
+
+                  <div className="extra mt-1 row justify-content-between">
+                    <div className="col-10">
+                      <div className="form-check">
+                        <input
+                          className="form-check-input"
+                          type="checkbox"
+                          id="remember"
+                          checked={remember}
+                          onChange={(e) => setRemember(e.currentTarget.checked)}
+                        />
+                        <label className="form-check-label" htmlFor="remember">
+                          Remember Me
+                        </label>
+                      </div>
+                    </div>
+                    <div className="col-0">
+                      <div className="lupa-password text-end">
+                        <Link to="/lupa-password">Lupa Password?</Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="text-end">
+                  < button color="cyan" appearance="primary"
+                     type="submit"
+                  >
+                    LOGIN
+                  </button>
+                </div>
+              </form>
+
+              <hr />
+              <div className="auth-option text-center  pt-0">
+              You Don’t Have Account? {" "}
+                <Link className="text-link" to="/register">
+                Register Now!!{" "}
+                </Link>
+                </div>
+                </div>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
